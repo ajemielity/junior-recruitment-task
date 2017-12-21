b0256e819041162fbae8bfc6b1b648790f53b379
Aleksandra Jemielity <ajemielity@o2.pl>
2017-12-21 20:03:35 +0100

    working version of list & add text area when user clicked plus-button

---
+++ frontend/index.js
@@ -0,0 +1,8 @@
+/*add text area when user clicked plus-button*/
+
+addTextArea = () => {
+  console.log('click')
+/*  document.getElementsByClassName('add-text-area').style.display = "block"*/
+  document.getElementById('add-text-area').style.display = "block"
+/*  document.getElementById('add-text-area').setAttribute("type", "text");*/
+}
