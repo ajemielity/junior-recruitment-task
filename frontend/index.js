bd9dde294f0fa42f82078e2d21b18e7905f1ed40
Aleksandra Jemielity <ajemielity@o2.pl>
2017-12-22 11:33:26 +0100

    mark items as done/undone

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,22 @@
-/*ADD TEXT AREA WHEN USER CLICK PLUS-BUTTON*/
+/* ADD TEXT AREA WHEN USER CLICK PLUS-BUTTON */
 addTextArea = () => {
   document.getElementById('add-text-area').style.display = "block"
 }
 
-/*DELETE ITEM FROM LIST*/
-deleteItem = (e) => {   /*e it's clicked button*/
-  e.parentElement.remove()   /*remove parent of the button*/
+/* DELETE ITEM FROM LIST */
+deleteItem = (e) => {   /* e it's clicked button */
+  e.parentElement.remove()    /* remove parent of the button (li) */
+}
+
+
+/*MARK ITEM AS DONE/UNDONE*/
+clickCheckbox = (e) => {    /* e it's clicked checkbox */
+  const parent = e.parentElement    /* parent of the checkbox = li */
+  if (e.checked) {    /*change li styles if checkbox is checked */
+    parent.style.color = "#9eb2c0"
+    parent.style.textDecoration = "line-through"
+  } else {
+    parent.style.color = "#2e3641"
+    parent.style.textDecoration = "none"
+  }
 }
