d4ff09042cf4e37d6cd7cf99c2f530ac4eec479c
Aleksandra Jemielity <ajemielity@o2.pl>
2017-12-22 12:16:34 +0100

    fix addTextArea function

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,19 @@
+/*GET TO-DO ITEMS FROM API*/
+const url = "https://todo-simple-api.herokuapp.com/todos?page=1&page_size=10"   //Path /todos?page=1&page_size=10 - Fetch all todos
+const xhr  = new XMLHttpRequest()
+let toDoItems
+xhr.open('GET', url, true)  // true for asynchronous
+xhr.onreadystatechange = function () {    //function to be executed when the readyState changes
+  if (xhr.readyState === 4 && xhr.status === 200) {   //readyState === 4 means DONE, operation is complete, status === 200 means "OK"
+    toDoItems = toDoItems = JSON.parse(xhr.responseText)    //change responses from text to objects
+    return toDoItems
+  } else {
+    return false
+  }
+}
+xhr.send(null);
+
+
 /* ADD TEXT AREA WHEN USER CLICK PLUS-BUTTON */
 addTextArea = () => {
   document.getElementById('add-text-area').style.display = "block"
@@ -21,16 +37,3 @@ clickCheckbox = (e) => {    /* e it's clicked checkbox */
   }
 }
 
-
-/*GET TO-DO ITEMS FROM API*/
-const url = "https://todo-simple-api.herokuapp.com/todos?page=1&page_size=10"
-const xhr  = new XMLHttpRequest()
-let toDoItems
-xhr.open('GET', url, true)  // true for asynchronous
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    toDoItems = toDoItems = JSON.parse(xhr.responseText); //change responses from text to objects
-    return toDoItems
-  }
-}
-xhr.send(null);
